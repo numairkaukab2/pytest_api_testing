614d8a808b4ec629f5a27c34f1360cea3957afbb
Numair <numairkaukab2@gmail.com>
2025-05-26 09:17:37 +0500

    Add Tests

--- tests/test_login.py
+++ tests/test_login.py
@@ -1,8 +1,22 @@
 import requests
+import pytest
 
 URL = "http://reqres.in"
 
-def test_get_users():
-    response = requests.get(URL + '/api/users?page=2')
+@pytest.fixture
+def headers():
+    return {"x-api-key":"reqres-free-v1"}
+
+
+def test_get_users(headers):
+    response = requests.get(URL + '/api/users?page=2', headers=headers)
     assert response.status_code == 200
-    
+
+def test_get_single_user(headers):
+    response = requests.get(URL + "/api/users/2", headers=headers)
+    assert response.status_code == 200
+    assert response.json()["data"]["id"] == 2
+
+def test_user_not_found(headers):
+    response = requests.get(URL + "/api/users/23", headers=headers)
+    assert response.status_code == 404
